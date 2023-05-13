1515f64183178c5ad0b1da78c9cd2dd320b3bf46
Aki Inoyatov <info@gravityelements.com>
2023-05-12 17:07:18 -0700

    figma navbar deployed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NavBar from './components/NavBar/NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      
+      <NavBar />
+    </div>
+    
   </React.StrictMode>
 );
 
